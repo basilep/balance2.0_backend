379bfb65cd8a4dd00e3db8b009923777d2f11bdd
basile <basile.perreaux@ulb.be>
2022-10-09 22:15:59 +0200

    docker working requirements.txt move to balance

--- balance/balance/settings.py
+++ balance/balance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rco)&5iy(_61a10cn0xsaqz(_whqax*_9fkdkjednv!xq$6jrf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
